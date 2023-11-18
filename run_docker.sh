b5b693e78ada7129b1b499f7053099f253ef3399
winkelstein <andrew.winkelstein@gmail.com>
2023-11-18 18:49:07 +0300

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker rmi -f && docker build --tag winkelstein_docker . && docker run -it winkelstein_docker zsh
+docker rmi -f winkelstein_docker && docker build --tag winkelstein_docker . && docker run -it winkelstein_docker zsh
